d4a9085f8f154be02e97f88003260254bfd56707
Nefi Garcia Gutierrez <nefigarcia@ibm.com>
2023-05-15 17:46:17 -0600

    sigup db

--- src/Paginas/Signup.js
+++ src/Paginas/Signup.js
@@ -13,9 +13,40 @@ import {
   from 'mdb-react-ui-kit';
 
 class Signup extends Component{
+
+    constructor(props){
+        super(props);
+        this.state={
+            nombre:'',
+            apellidos:'',
+            email:'',
+            contrasena:''
+        }
+        this.handleSubmit=this.handleSubmit.bind(this);
+
+    }
+
+    handleSubmit(e){
+        e.preventDefault();
+        const{nombre,apellidos,email,contrasena}=this.state;
+        if(!nombre && apellidos)
+        return;
+        this.registrar(nombre,apellidos,email,contrasena);
+    }
+    registrar(nombre,apellidos,email,contrasena){
+        let dat={nombre:nombre,apellidos:apellidos,email:email,contrasena:contrasena};
+        return fetch('http://localhost:3001/Signup',{
+               method:'POST',
+               mode:'cors',
+               body:JSON.stringify(dat),
+               headers:{'content-type':'application/json'},
+            })
+            .then(res=>res.json())
+            .catch(err=>err);
+    }
     render(){
         return (
-            <MDBContainer fluid className='my-5'>
+            <MDBContainer onClick={this.handleSubmit} fluid className='my-5'>                      
         
               <MDBRow className='g-0 align-items-center'>
                 <MDBCol col='6'>
@@ -27,16 +58,16 @@ class Signup extends Component{
         
                       <MDBRow>
                         <MDBCol col='6'>
-                          <MDBInput wrapperClass='mb-4' label='First name' id='form1' type='text'/>
+                          <MDBInput wrapperClass='mb-4' label='Nombre' id='form1' type='text' name='nombre'/>
                         </MDBCol>
         
                         <MDBCol col='6'>
-                          <MDBInput wrapperClass='mb-4' label='Last name' id='form2' type='text'/>
+                          <MDBInput wrapperClass='mb-4' label='Apellidos' id='form2' type='text'/>
                         </MDBCol>
                       </MDBRow>
         
-                      <MDBInput wrapperClass='mb-4' label='Email' id='form3' type='email'/>
-                      <MDBInput wrapperClass='mb-4' label='Password' id='form4' type='password'/>
+                      <MDBInput wrapperClass='mb-4' label='Email' id='form3' type='email' name='email'/>
+                      <MDBInput wrapperClass='mb-4' label='Contrasena' id='form4' type='password' name='contrasena'/>
         
                       <div className='d-flex justify-content-center mb-4'>
                         <MDBCheckbox name='flexCheck' value='' id='flexCheckDefault' label='Subscribe to our newsletter' />

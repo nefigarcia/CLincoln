b7f16fb6f7b5788c2a2ec9c04404482272d1bea9
Nefi Garcia Gutierrez <nefigarcia@ibm.com>
2023-05-24 09:14:30 -0600

    Inicio landpage

--- src/Paginas/Inicio.js
+++ src/Paginas/Inicio.js
@@ -1,23 +1,101 @@
 import React,{Component} from "react";
+import rectangle_1 from '../Fotos/Rectangle_1.jpg';
+import rectangle_2 from '../Fotos/Rectangle_2.png';
+
+import Rectangle_3 from '../Fotos/Rectangle_3.jpg';
+import Rectangle_4 from '../Fotos/Rectangle_4.jpg';
+import Rectangle_5 from '../Fotos/Rectangle_5.png';
+import Hero from "../components/Hero";
+import Step from "../components/Step";
+import BottomLead from "../components/BottomLead";
+
 import { InfoConsumer } from "../context";
 
-class Inicio extends Component{
-    state={estaMenu:true}
-    render(){
-        return(
-            <InfoConsumer>
-            {data=>{                
-
-                return(
-                    <div> Rosystems.
-                    </div>
-                   
-                );
-            }}
-        </InfoConsumer>
-        );
+ function Inicio(){
+    const data = {
+        hero:{
+          appType: 'Food app',
+          tagLine: 'Why stay hungry when you can order from Bella Onojie',
+          description: 'Download the bella onoje’s food app now on',
+          mainActionText: 'Playstore',
+          extraActionText: 'App Store',
+        },
+        step1: {
+          title: 'Create an account',
+          heading: 'Create/login to an existing account to get started',
+          description: 'An account is created with your email and a desired password',
+          img: Rectangle_3,
+          alternate: false,
+        },
+        step2: {
+          title: 'Explore while shopping',
+          heading: 'Shop for your favorites meal as e dey hot.',
+          description: 'Shop for your favorite meals or drinks and enjoy while doing it.',
+          img: Rectangle_4,
+          alternate: true,
+        },
+        step3: {
+          title: 'Checkout',
+          heading: "When you're done, check out and get it delivered.",
+          description: "When you're done, check out and get it  delivered with ease.",
+          img: Rectangle_5,
+          alternate: false,
+        },
+        bottomLead: {
+          actionText: 'Download the app now.',
+          description: 'Available on your favourite store. Start your premium experience now.',
+          mainActionText: 'Playstore',
+          extraActionText: 'App Store',
+        },
+      }
+      return (
+        // __________________________TODO: ____________________
+        // Add Montserrat font to everything (body)
         
-    }
-}
-
-export default Inicio;
+        <div className="box-border">
+          <div className="flex flex-col">
+            <Hero 
+              appType={data.hero.appType}
+              tagLine={data.hero.tagLine}
+              description={data.hero.description}
+              mainActionText={data.hero.mainActionText}
+              extraActionText={data.hero.extraActionText}
+            />
+            
+            <div id="divider" className="rounded-full ring-2 ring-gray-200 lg:w-1/2 lg:mx-auto "></div>
+            
+            <div id="faq" className="pt-20 mb-20 text-3xl font-semibold text-center text-blue-800 lg:font-bold">How the app works </div>
+            
+            <Step
+              title={data.step1.title}
+              heading={data.step1.heading}
+              description={data.step1.description}
+              img={data.step1.img}
+              alternate={data.step1.alternate}
+              />
+              <Step
+              title={data.step2.title}
+              heading={data.step2.heading}
+              description={data.step2.description}
+              img={data.step2.img}
+              alternate={data.step2.alternate}
+              />
+              <Step
+              title={data.step3.title}
+              heading={data.step3.heading}
+              description={data.step3.description}
+              img={data.step3.img}
+              alternate={data.step3.alternate}
+              />
+              
+              <BottomLead 
+                actionText={data.bottomLead.actionText}
+                description={data.bottomLead.description}
+                mainActionText={data.bottomLead.mainActionText}
+                extraActionText={data.bottomLead.extraActionText}
+              />
+          </div>
+        </div>
+      );
+ }
+ export default Inicio;
